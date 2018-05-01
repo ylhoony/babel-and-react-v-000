a2b8d82fb0e294d527c53289bd560130f6ea1d9c
Hoon Lee <yl.hoony@gmail.com>
2018-05-01 03:53:59 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+var profile = (
+  <div>
+    <img src="avatar.png" className="profile" />
+    <h3>{[user.firstName, user.lastName].join(' ')}</h3>
+  </div>
+)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
